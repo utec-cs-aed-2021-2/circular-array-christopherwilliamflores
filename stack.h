d78d1b3a06820f69563e9b077a827b6d18bbeaf2
Christopher William Flores Rimac <82064413+christopherwilliamflores@users.noreply.github.com>
2021-09-14 12:03:43 -0500

    stack refresh

--- stack.h
+++ stack.h
@@ -2,5 +2,20 @@
 
 template <typename T>
 class StackArray : public CircularArray<T> { 
-    
+public:
+    Stack () : CircularArray<T>(){
+
+    }
+    Stack (int c) : CircularArray<T>(c) {
+
+    }
+
+    void push (int val) {
+        this->push_back(val);
+    }
+
+    T pop () {
+        return this->pop_back();
+    }
 };
+#endif //LISTACIRCULAR_STACK_H
